419744c9b5c38265978bf14f5d2ccbcc49c8570a
23kimbio <kimbiofrank81@gmail.com>
2024-06-20 21:29:25 +0300

    python class

---
+++ StringVariables/exibits/LoopsFor.py
@@ -0,0 +1,8 @@
+multiplyBy = 2
+for current_range_number in range(1, 11):
+    answer = current_range_number * multiplyBy
+    #print(str(current_range_number) +"times " + str(multiplyBy) + "= " + str(answer))
+
+superheros = ["Frank Kimbio", "Matt Murrdock", "David Ouma"]
+for hero in superheros:
+    print(hero)
---
+++ StringVariables/exibits/PythonFunction.py
@@ -0,0 +1,14 @@
+def error_message():
+    print("Something went wrong")
+
+
+def addition(first_number, second_number):
+    answer = first_number + second_number
+    return answer
+
+
+#error_message()
+
+additionTotal = addition(3, 9)
+
+print("Number to add is: ", additionTotal)
